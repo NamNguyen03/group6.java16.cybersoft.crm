fa8ba27599ab5d89d08136882f863e2c4e1c4444
Nam Nguyen <nam03031999@gmail.com>
2022-03-10 02:25:27 -0400

    fix project

--- crm/src/main/java/group6/java16/cybersoft/javabackend/crm/service/project/ProjectService.java
+++ crm/src/main/java/group6/java16/cybersoft/javabackend/crm/service/project/ProjectService.java
@@ -5,6 +5,7 @@ package group6.java16.cybersoft.javabackend.crm.service.project;
 
 import java.util.List;
 
+import group6.java16.cybersoft.javabackend.crm.service.project.ProjectResponseModels.ProjectResponse;
 import group6.java16.cybersoft.javabackend.crm.service.project.ProjectResponseModels.ProjectRoleResponse;
 
 /**
@@ -19,5 +20,13 @@ public interface ProjectService {
 	 * @return 	List<ProjectRoleResponse> 
 	 */
 	List<ProjectRoleResponse> getAllProjectByLeaderIsNull();
+
+	/**
+	 * get all project user have any permission
+	 * 
+	 * @param usernameReq
+	 * @return List<ProjectRoleResponse>
+	 */
+	List<ProjectResponse> getAllMyProject(String usernameReq);
 
 }
